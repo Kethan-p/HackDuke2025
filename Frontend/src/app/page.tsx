9a46ae81950d9c42ab3e26af2b62d27c689d845d
Daniel Cohen <djc86@duke.edu>
2025-02-08 13:24:54 -0500

    Fixed yarn build issue

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,11 +1,22 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import MapPage from './pages/MapPage';
 
 const Home: React.FC = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    // Alternatively, you could return a loading spinner here.
+    return null;
+  }
+
   return (
     <Router>
       <Routes>
